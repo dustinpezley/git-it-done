d2e951cdbbe83c34e5eaa603b00f37f2adbdd043
dustinpezley <dustinpezley@gmail.com>
2022-06-07 11:19:02 -0500

    featured language functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
   // format the github api url
@@ -82,3 +83,28 @@ var displayRepos = function(repos, searchTerm) {
     repoContainerEl.appendChild(repoEl);
   }
 }
+
+getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q="+language+"+is:featured$sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(data) { 
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: GitHub User Not Found")
+    }
+  });
+}
+var buttonClickHandler = function(e) {
+  var language = e.target.getAttribute("data-language");
+  if(language) {
+    getFeaturedRepos(language);
+  }
+
+  repoContainerEl.textContent="";
+}
+
+
+languageButtonsEl.addEventListener("click",buttonClickHandler);
