46b1e20580ef474bdca49f852a4e76610b2c35f8
dustinpezley <dustinpezley@gmail.com>
2022-06-07 10:55:54 -0500

    linked pages and error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -53,8 +53,9 @@ var displayRepos = function(repos, searchTerm) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     //create a container for each
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo="+repoName)
 
     //create a span to hold repo name
     var titleEl = document.createElement("span");
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,8 +1,21 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if(repoName){ 
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+  } else {
+    document.location.replace("./index.html");
+  }
+}
 
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/"+repo+"/issues?direction=asc";
+
   fetch(apiUrl).then(function(response) {
     if(response.ok) {
       response.json().then(function(data) {
@@ -14,7 +27,7 @@ var getRepoIssues = function(repo) {
         }
       });
     } else {
-      alert("There was a problem with your request!");
+      document.location.replace(".index.html");
     }
   });
 }
@@ -68,4 +81,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
